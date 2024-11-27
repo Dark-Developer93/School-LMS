c1e612e5ef500c011e130fbc16af9f742359eb13
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-11-28 00:09:52 +0200

    refactor(dashboard layout): Include navigation components and improve structure

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,30 @@
-export default function DashboardLayout(
-  {
-    // children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>
-) {
+import Image from "next/image";
+import Link from "next/link";
+
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div className="h-screen flex">
-      {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        LEFT
+    <div className="flex h-screen">
+      <div className="w-[14%] p-4 md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link
+          href="/"
+          className="flex items-center justify-center gap-2 lg:justify-start"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">School LMS</span>
+        </Link>
+        <Menu />
       </div>
-      {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-        RIGHT
+
+      <div className="w-[86%] overflow-scroll bg-[#F7F8FA] md:w-[92%] lg:w-[84%] xl:w-[86%]">
+        <Navbar />
+        {children}
       </div>
     </div>
   );

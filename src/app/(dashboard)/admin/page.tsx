ff2c3257fcd1f73042d602a0bdc13f8e8f902b2d
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-11-29 00:52:03 +0200

    feature(admin-page): integrate Announcements and EventCalendar components into AdminPage layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,14 +1,16 @@
-import InfoCard from "@/components/info-card/InfoCard";
-import RadialBarChart from "@/components/charts/RadialBarChart";
+import Announcements from "@/components/announcements/Announcements";
 import BarChart from "@/components/charts/BarChart";
 import LineChart from "@/components/charts/LineChart";
+import RadialBarChart from "@/components/charts/RadialBarChart";
+import EventCalendar from "@/components/event-calendar/EventCalendar";
+import InfoCard from "@/components/info-card/InfoCard";
 
 const AdminPage = () => {
   return (
     <div className="flex flex-col gap-4 p-4 md:flex-row">
       {/* LEFT */}
-      <div className="flex w-full flex-col gap-4 lg:w-2/3">
-        {/* INFO CARDS */}
+      <div className="flex w-full flex-col gap-8 lg:w-2/3">
+        {/* USER CARDS */}
         <div className="flex flex-wrap justify-between gap-4">
           <InfoCard type="student" />
           <InfoCard type="teacher" />
@@ -17,20 +19,25 @@ const AdminPage = () => {
         </div>
         {/* MIDDLE CHARTS */}
         <div className="flex flex-col gap-4 lg:flex-row">
+          {/* COUNT CHART */}
           <div className="h-[450px] w-full lg:w-1/3">
             <RadialBarChart />
           </div>
+          {/* ATTENDANCE CHART */}
           <div className="h-[450px] w-full lg:w-2/3">
             <BarChart />
           </div>
         </div>
-        {/* BOTTOM CHARTS */}
-        <div className="flex flex-col">
+        {/* BOTTOM CHART */}
+        <div className="h-[500px] w-full">
           <LineChart />
         </div>
       </div>
       {/* RIGHT */}
-      <div className="flex w-full flex-col gap-4 lg:w-1/3">RIGHT</div>
+      <div className="flex w-full flex-col gap-8 lg:w-1/3">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };

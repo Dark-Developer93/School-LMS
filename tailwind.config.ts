0f9da3a64e72a9818664055964fcda09cd440c90
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-11-28 23:13:05 +0200

    feat(dependencies): add recharts library and update Tailwind CSS color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        sky: "#C3EBFA",
+        "sky-light": "#EDF9FD",
+        purple: "#CFCEFF",
+        "purple-light": "#F1F0FF",
+        yellow: "#FAE27C",
+        "yellow-light": "#FEFCE8",
+      },
     },
   },
   plugins: [],

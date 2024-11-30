34f14f3b6c0162894aef980139797ab4ebd63e67
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-11-30 02:32:53 +0200

    feature(teacher): enhance TeacherPage layout with calendar and announcements

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,5 +1,22 @@
+import Announcements from "@/components/announcements/Announcements";
+import BigCalendar from "@/components/calendars/BigCalendar";
+
 const TeacherPage = () => {
-  return <div>Teacher Page</div>;
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full rounded-md bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="flex w-full flex-col gap-8 xl:w-1/3">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;

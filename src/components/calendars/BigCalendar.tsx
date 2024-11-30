8c88215a33e4197c70206457076427d39c1c84fc
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-11-30 02:32:12 +0200

    feature(calendar): add BigCalendar component with customizable views and event handling

---
+++ src/components/calendars/BigCalendar.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Calendar, dateFnsLocalizer, View, Views } from "react-big-calendar";
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+  const [date, setDate] = useState(new Date());
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  const handleNavigate = (newDate: Date) => {
+    setDate(newDate);
+  };
+
+  return (
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
+      date={date}
+      style={{ height: "98%" }}
+      onView={handleOnChangeView}
+      onNavigate={handleNavigate}
+      onSelectSlot={({ start }) => {
+        setDate(start);
+        setView(Views.DAY);
+      }}
+      selectable
+      min={new Date(2025, 1, 0, 8, 0, 0)}
+      max={new Date(2025, 1, 0, 17, 0, 0)}
+    />
+  );
+};
+
+export default BigCalendar;

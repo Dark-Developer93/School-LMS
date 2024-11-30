228d1e82438c5e29b90c7fd7550163112ae3bb91
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-11-30 02:33:07 +0200

    feature(parent): enhance ParentPage layout with calendar and announcements

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,26 @@
+import Announcements from "@/components/announcements/Announcements";
+import BigCalendar from "@/components/calendars/BigCalendar";
+
 const ParentPage = () => {
-  return <div>Parent Page</div>;
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full rounded-md bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+        <div className="h-full rounded-md bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule (Jane Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="flex w-full flex-col gap-8 xl:w-1/3">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default ParentPage;

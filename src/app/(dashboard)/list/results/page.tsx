fc8e4794cd438a47b4464da0091656b089bff98d
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-12-01 17:07:20 +0200

    feature(results): add ResultListPage component with table, search, and pagination

---
+++ src/app/(dashboard)/list/results/page.tsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+
+import FormModal from "@/components/forms/FormModal";
+import Pagination from "@/components/pagination/Pagination";
+import { resultsData, role } from "@/lib/data";
+import { Result } from "@/types";
+import TableSearch from "@/components/table/TableSearch";
+import Table from "@/components/table/Table";
+
+const columns = [
+  {
+    header: "Subject Name",
+    accessor: "name",
+  },
+  {
+    header: "Student",
+    accessor: "student",
+  },
+  {
+    header: "Score",
+    accessor: "score",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const ResultListPage = () => {
+  const renderRow = (item: Result) => (
+    <tr
+      key={item.id}
+      className="hover:bg-lamaPurpleLight border-b border-gray-200 text-sm even:bg-slate-50"
+    >
+      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td>{item.student}</td>
+      <td className="hidden md:table-cell">{item.score}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.date}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" ||
+            (role === "teacher" && (
+              <>
+                <FormModal table="result" type="update" data={item} />
+                <FormModal table="result" type="delete" id={item.id} />
+              </>
+            ))}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="m-4 mt-0 flex-1 rounded-md bg-white p-4">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden text-lg font-semibold md:block">All Results</h1>
+        <div className="flex w-full flex-col items-center gap-4 md:w-auto md:flex-row">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button
+              type="button"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-yellow"
+            >
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button
+              type="button"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-yellow"
+            >
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {(role === "admin" || role === "teacher") && (
+              <FormModal table="result" type="create" />
+            )}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={resultsData} />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default ResultListPage;
